adab57e4952d31a4b99b27f5fd7231d008ad2c9a
Aktar <pabelchowdury9@gmail.com>
2024-08-27 23:51:02 +0600

    add Manrope font

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,10 @@ module.exports = {
   
         '2xl': '1536px',
         // => @media (min-width: 1536px) { ... }
-      extend: {},
+      extend: {
+        fontFamily: {
+          sans: ['Manrope', 'sans-serif'],
+      },
     },
     plugins: [],
   }
